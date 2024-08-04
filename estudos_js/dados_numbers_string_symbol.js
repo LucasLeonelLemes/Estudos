8343b881ba5ece0eb6da1112ec4d0fb662f33b1a
Lucas Santos <lucas701815@gmail.com>
2024-08-04 12:59:41 -0300

    conhecemos mais sobre numeros

---
+++ estudos_js/dados_numbers_string_symbol.js
@@ -0,0 +1,41 @@
+/*Tipos de dados, Numbers, String e Symbol
+Aprendendo Variáveis, Escopo e Tipos de dados no JavaScript
+
+<----------------------------- // --------------------------------------->
+
+Tipos de numbers: + Infinity, - Infinity e NaN
+" tudo que tem numero etipos de numeros ele vao ser numbers"
+
+"Double Precision 64-bit binary format IEEE754"
+quase todas as liguagens que usamos eles sao baseados nesses dados numericos
+mas pode ocorrer alguns erros pois oque o ser humano esta acostumado a usar e o formato decimal e os computadores usam o binario
+
+exemplos: 
+10+10 = 20 CORRETO
+
+10-10 = 0 CORRETO
+
+0.01 - 0.03 = 0.01999999999 ERRADO
+
+"isso daqui e mais ou menos a sujeira que e gerada durante a operacao binaria"
+
+entao se tiver que fazer contas bem precisas, cientificas ou algo parecido que nao possa ocorrer falhas, temos que recorrer para outro tipo de representacao
+
+esse site e bom dar uma olhada que ele fala mais ou menos sobre esse problema e mostra em varias linguagens que esse problema existe: */"$https://0.30000000000000004.com/" /*
+
+umas das solucoes para nao ter esse problema e usando o Decimal.js */ "https://mikemcl.github.io/decimal.js/" /*
+
+
+resumo dos Numbers: tudo e numero ate que se prove ao contrario, mais ou menos isso, mas temos que se cuidar com isso 
+que ele baseia para fazer os calculos "Double Precision 64-bit binary format IEEE754", pois se prisarmos de precisao
+sem ter essas falhas de arredondamentos por nos trabalhos em decimal e a maquina em binaria, temos que usar ferramentas para nao ter esse problema.
+
+
+
+<---------------------------------------------- // --------------------------------------->
+
+paramos das strings 
+no minuto 9:20 do video.
+
+
+*/
